09847ef52fa9982c09a1da1c8ad8e977a392f7f7
Daniel Steuernol <daniel@sendwithus.com>
2018-02-05 20:02:20 -0800

    fix elm build

--- assets/src/utils/elm.ts
+++ assets/src/utils/elm.ts
@@ -1,16 +1,16 @@
-import {entries, map} from './collections';
+import { entries, map } from "./collections";
 
-export function embedApp<T>(
+export function embedApp<T, Options>(
   tag: string,
-  App: Elm.App<T>,
-  config?: Elm.MetaData
+  App: Elm.App<T, Options>,
+  config?: any
 ): Elm.Program<T>[] {
   const elements = entries(document.querySelectorAll(tag));
 
   return map(elements, element => {
     const attrs = entries(element.attributes);
 
-    let options: Elm.MetaData = {};
+    let options: any = {};
 
     for (const a of attrs) {
       options[a.name] = a.value;
