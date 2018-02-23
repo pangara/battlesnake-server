80744ced30f36004ededdb746dc51242df1aec35
Daniel Steuernol <daniel@sendwithus.com>
2018-02-23 11:11:13 -0800

    constrict taunt display

--- lib/bs/game_state.ex
+++ lib/bs/game_state.ex
@@ -90,7 +90,6 @@ defmodule Bs.GameState do
       |> Death.reap()
       |> Map.update!(:world, &World.step/1)
 
-    IO.inspect(state)
     if done?(state), do: step_done(state), else: state
   end
 
